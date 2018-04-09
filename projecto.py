94b656436e7e7493fe684b698d19aac04ac42ffa
JoseFernandoInsper <josefmc@al.edu.br>
2018-04-09 20:20:55 -0300

    ajuste camera do pc

--- projecto.py
+++ projecto.py
@@ -109,7 +109,7 @@ if __name__=="__main__":
 
 	rospy.init_node("cor")
 	# Para usar a Raspberry Pi
-	recebedor = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=1, buff_size = 2**24)
+	recebedor = rospy.Subscriber("/cv_camera/image_raw/compressed", CompressedImage, roda_todo_frame, queue_size=1, buff_size = 2**24)
 	
 	# Para usar a webcam 
 	#recebedor = rospy.Subscriber("/cv_camera/image_raw/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
